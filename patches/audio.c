705721837bb330e5dd8d21d714cb745ea58e3de5
Sonic Dreamcaster <alejandro.asenjo88@gmail.com>
2025-04-24 11:25:54 -0300

    fix audio thread hang on musicTrack3Play

--- patches/audio.c
+++ patches/audio.c
@@ -95,3 +95,51 @@ RECOMP_PATCH void musicTrack2Play(s32 track) __attribute__((optnone)) {
     alCSPPlay(g_musicXTrack2SeqPlayer);
 }
 #endif
+
+#if 1
+RECOMP_PATCH void musicTrack3Play(s32 track) __attribute__((optnone)) {
+    u32 trackSizeBytes;
+    struct music_struct_b thing;
+    u8* temp_a0;
+    void* romAddress;
+    u32 t3;
+    struct huft hlist;
+
+    if (g_sndBootswitchSound) {
+        return;
+    }
+
+    if (g_musicXTrack3CurrentTrackNum) {
+        musicTrack3Stop();
+    }
+
+    g_musicXTrack3CurrentTrackNum = track;
+
+    while (alCSPGetState(g_musicXTrack3SeqPlayer)) {
+        // @recomp: Yield the thread so it doesn't stall
+        yield_self_1ms();
+    }
+
+    romAddress = g_musicDataTable->seqArray[g_musicXTrack3CurrentTrackNum].address;
+
+    if (romAddress < (void*) ROM_MUSIC_START_OFFSET) {
+        // Note: recursive call
+        musicTrack3Play(M_SHORT_SOLO_DEATH);
+
+        return;
+    }
+
+    t3 = ALIGN16_a(g_musicTrackLength[g_musicXTrack3CurrentTrackNum]) + ALIGN16_a(NUM_MUSIC_TRACKS);
+    trackSizeBytes = ALIGN16_a(g_musicTrackCompressedLength[g_musicXTrack3CurrentTrackNum]);
+    thing.seqData = g_musicXTrack3SeqData;
+    temp_a0 = (u8*) ((t3 + (s32) thing.seqData) - trackSizeBytes);
+
+    romCopy(temp_a0, romAddress, trackSizeBytes);
+    decompressdata(temp_a0, thing.seqData, &hlist);
+
+    alCSeqNew(&g_musicXTrack3Seq, g_musicXTrack3SeqData);
+    alCSPSetSeq(g_musicXTrack3SeqPlayer, &g_musicXTrack3Seq);
+    musicTrack3ApplySeqpVol(musicTrack3GetVolume());
+    alCSPPlay(g_musicXTrack3SeqPlayer);
+}
+#endif
